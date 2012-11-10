d77e43b8b6a13228486577388da94e7c28ffda51
mark zhou <winloud@163.com>
2012-11-09 17:23:52 -0700

    Bokai

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '4f5ee4757828c71d4f8435c6d75690572ca16714d0648ad481efab321605d16296568a77d1a283d0c2b16b62f74eedf40566a467a5606a7c2caa6645a4ca88dc'
